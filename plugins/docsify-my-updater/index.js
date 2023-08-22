8ca8c1e8565b8322c1aeb69214929a0325bbdf9d
yonatan <yonatan.lin@outlook.com>
2023-08-23 01:51:02 +0800

    chore

--- plugins/docsify-my-updater/index.js
+++ plugins/docsify-my-updater/index.js
@@ -10,22 +10,17 @@ function plugin(hook, vm) {
     let readTime = Math.ceil(wordsCount / 400) + " min read"
 
     // 使用 GitHub API 获取文件提交时间
-    let commitDate = '-'
-    try {
-      let isGithubFilePath = /raw.githubusercontent.com/g.test(vm.route.file)
-      // +4 是跳过了匹配项, owner, repo, branch(sha)
-      let pathIndex = vm.route.file.split('/').findIndex(a => a == 'raw.githubusercontent.com') + 4
-      let filePath = isGithubFilePath ? vm.route.file.split('/').slice(pathIndex).join('/') : vm.route.file
-      let owner = vm.route.file.split('/')[pathIndex - 3]
-      let repo = vm.route.file.split('/')[pathIndex - 2]
-      let date_url = `https://api.github.com/repos/${owner}/${repo}/commits?per_page=1&path=${filePath}`
-      let response = await fetch(date_url)
-      let commits = await response.json()
-      let date = commits[0]['commit']['committer']['date'];
-      commitDate = window.$docsify.formatUpdated(date);
-    } catch (error) {
-      console.log(error);
-    }
+    let isGithubFilePath = /raw.githubusercontent.com/g.test(vm.route.file)
+    // +4 是跳过了匹配项, owner, repo, branch(sha)
+    let pathIndex = vm.route.file.split('/').findIndex(a => a == 'raw.githubusercontent.com') + 4
+    let filePath = isGithubFilePath ? vm.route.file.split('/').slice(pathIndex).join('/') : vm.route.file
+    let owner = vm.route.file.split('/')[pathIndex - 3]
+    let repo = vm.route.file.split('/')[pathIndex - 2]
+    let date_url = `https://api.github.com/repos/${owner}/${repo}/commits?per_page=1&path=${filePath}`
+    let response = await fetch(date_url)
+    let commits = await response.json()
+    let date = commits[0]['commit']['committer']['date'];
+    commitDate = window.$docsify.formatUpdated(date) || '-';
 
     // let text2 = `<p style="color:#808080;font-size:14px;">${author} · {docsify-updated} · ${wordsStr} · ${readTime}</p>`
     let text2 = `<p style="color:#808080;font-size:14px;">${author} · ${commitDate} · ${readTime}</p>`
