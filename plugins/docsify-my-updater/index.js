55e8e7c3aa6ec5d9c5573d8393a45c365f431dd6
yonatan <yonatan.lin@outlook.com>
2023-08-23 02:55:29 +0800

    chore

--- plugins/docsify-my-updater/index.js
+++ plugins/docsify-my-updater/index.js
@@ -1,39 +1,42 @@
 function plugin(hook, vm) {
 
   hook.beforeEach(function (content) {
-
-    let author = vm.config.name
-
-    // Match regex every time you start parsing .md
-    let wordsCount = content.match(/([\u4e00-\u9fa5]+?|[a-zA-Z0-9]+)/g).length
-    // let wordsStr = wordsCount + " words"
-    let readTime = Math.ceil(wordsCount / 400) + " min read"
-
-    // 使用 GitHub API 获取文件提交时间
-    let isGithubFilePath = /raw.githubusercontent.com/g.test(vm.route.file)
-    // +4 是跳过了匹配项, owner, repo, branch(sha)
-    let pathIndex = vm.route.file.split('/').findIndex(a => a == 'raw.githubusercontent.com') + 4
-    let filePath = isGithubFilePath ? vm.route.file.split('/').slice(pathIndex).join('/') : vm.route.file
-    let owner = vm.route.file.split('/')[pathIndex - 3]
-    let repo = vm.route.file.split('/')[pathIndex - 2]
-    let date_url = `https://api.github.com/repos/${owner}/${repo}/commits?per_page=1&path=${filePath}`
-    fetch(date_url)
-      .then((response) => {
-        return response.json()
-      })
-      .then((commits) => {
-        let date = commits[0]['commit']['committer']['date'];
-        let commitDate = window.$docsify.formatUpdated(date);
-        // let text2 = `<p style="color:#808080;font-size:14px;">${author} · {docsify-updated} · ${wordsStr} · ${readTime}</p>`
-        let text2 = `<p style="color:#808080;font-size:14px;">${author} · ${commitDate} · ${readTime}</p>`
-        content.replace(/{docsify-my-updater}/g, text2)
-      })
-
-
-    let copyright = `\n<p style="color:#808080;font-size:14px;margin-top:40px;">本文作者为 <a href="https://yonatan.cn">Yonatan</a>，转载请注明出处：${window.location.href}</p>`
+    
     let isMatched = /{docsify-my-updater}/g.test(content);
+    if (isMatched) {
+      let author = vm.config.name
+      let copyright = `\n<p style="color:#808080;font-size:14px;margin-top:40px;">本文作者为 <a href="https://yonatan.cn">Yonatan</a>，转载请注明出处：${window.location.href}</p>`
 
-    return content + (isMatched ? copyright : "")
+      // Match regex every time you start parsing .md
+      let wordsCount = content.match(/([\u4e00-\u9fa5]+?|[a-zA-Z0-9]+)/g).length
+      // let wordsStr = wordsCount + " words"
+      let readTime = Math.ceil(wordsCount / 400) + " min read"
+
+      // 使用 GitHub API 获取文件提交时间
+      let isGithubFilePath = /raw.githubusercontent.com/g.test(vm.route.file)
+      if (isGithubFilePath) {
+        // +4 是跳过了匹配项, owner, repo, branch(sha)
+        let pathIndex = vm.route.file.split('/').findIndex(a => a == 'raw.githubusercontent.com') + 4
+        let filePath = isGithubFilePath ? vm.route.file.split('/').slice(pathIndex).join('/') : vm.route.file
+        let owner = vm.route.file.split('/')[pathIndex - 3]
+        let repo = vm.route.file.split('/')[pathIndex - 2]
+        let date_url = `https://api.github.com/repos/${owner}/${repo}/commits?per_page=1&path=${filePath}`
+        fetch(date_url)
+          .then((response) => {
+            return response.json()
+          })
+          .then((commits) => {
+            let date = commits[0]['commit']['committer']['date'];
+            let commitDate = window.$docsify.formatUpdated(date);
+            let text = `<p style="color:#808080;font-size:14px;">${author} · ${commitDate} · ${readTime}</p>`
+            return content.replace(/{docsify-my-updater}/g, text) + copyright
+          })
+      } else {
+        // let text = `<p style="color:#808080;font-size:14px;">${author} · {docsify-updated} · ${wordsStr} · ${readTime}</p>`
+        let text = `<p style="color:#808080;font-size:14px;">${author} · {docsify-updated} · ${readTime}</p>`
+        return content.replace(/{docsify-my-updater}/g, text) + copyright
+      }
+    }
   })
 
 }
