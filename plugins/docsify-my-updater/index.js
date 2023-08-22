fe6469a0660b21e0888ebb77d68685191535de9d
yonatan <yonatan.lin@outlook.com>
2023-08-23 02:22:51 +0800

    chore

--- plugins/docsify-my-updater/index.js
+++ plugins/docsify-my-updater/index.js
@@ -17,24 +17,23 @@ function plugin(hook, vm) {
     let owner = vm.route.file.split('/')[pathIndex - 3]
     let repo = vm.route.file.split('/')[pathIndex - 2]
     let date_url = `https://api.github.com/repos/${owner}/${repo}/commits?per_page=1&path=${filePath}`
-    let commitDate = (async () => {
-      await fetch(date_url)
+    fetch(date_url)
       .then((response) => {
         return response.json()
       })
       .then((commits) => {
         let date = commits[0]['commit']['committer']['date'];
-        return window.$docsify.formatUpdated(date);
+        let commitDate = window.$docsify.formatUpdated(date);
+        // let text2 = `<p style="color:#808080;font-size:14px;">${author} · {docsify-updated} · ${wordsStr} · ${readTime}</p>`
+        let text2 = `<p style="color:#808080;font-size:14px;">${author} · ${commitDate} · ${readTime}</p>`
+        content.replace(/{docsify-my-updater}/g, text2)
       })
-    })() || '-'
 
-    // let text2 = `<p style="color:#808080;font-size:14px;">${author} · {docsify-updated} · ${wordsStr} · ${readTime}</p>`
-    let text2 = `<p style="color:#808080;font-size:14px;">${author} · ${commitDate} · ${readTime}</p>`
 
     let copyright = `\n<p style="color:#808080;font-size:14px;margin-top:40px;">本文作者为 <a href="https://yonatan.cn">Yonatan</a>，转载请注明出处：${window.location.href}</p>`
     let isMatched = /{docsify-my-updater}/g.test(content);
 
-    return content.replace(/{docsify-my-updater}/g, text2) + (isMatched ? copyright : "")
+    return content + (isMatched ? copyright : "")
   })
 
 }
