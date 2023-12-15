7c974ccdf9401b33d42ab95006ba8a857e1f3326
yonatan <yonatan.lin@outlook.com>
2023-12-16 00:06:16 +0800

    chore

--- plugins/docsify-my-markmap/index.js
+++ plugins/docsify-my-markmap/index.js
@@ -1,15 +1,13 @@
 function appendScript() {
   const script = document.createElement('script');
   script.async = true;
-  script.src = 'https://cdn.jsdelivr.net/npm/markmap-autoloader@0.14.4';
+  script.src = '//cdn.jsdelivr.net/npm/markmap-autoloader@0.14.4';
   document.body.appendChild(script);
 }
 
 function replaceContent(content) {
   const regex = /<code class="lang-markmap">([\s\S]*?)<\/code>/;
   return content.replace(regex, (matched, optionsStr, code) => {
-    window.isRenderNote = true;
-    // window.isShowNote = false;
     return `
       <div class="markmap">
         <script type="text/template">
@@ -20,15 +18,9 @@ function replaceContent(content) {
   });
 }
 
-function matchContent(content) {
-  const regex = /<code class="lang-markmap">([\s\S]*?)<\/code>/;
-  return regex.exec(content)[1]
-}
-
 const myMarkMapPlugin = (hook, vm) => {
 
   hook.init(_ => {
-    window.isRenderNote = false;
     appendScript();
   });
 
@@ -36,29 +28,10 @@ const myMarkMapPlugin = (hook, vm) => {
 
     window.markmapHTML = html;
     next(replaceContent(html));
-    window.isShowNote = true;
-
-    // 添加一个重置按钮
-    // const reloadBtnHtml = `
-    //   <button id="markmap_reload_btn">reload</button>
-    // `;
-    // const reloadBtnEl = Docsify.dom.create('div', reloadBtnHtml);
-    // reloadBtnEl.addEventListener('click', _ => {
-    //   markmap.autoLoader.renderAll();
-    // })
-    // const markmapEl = Docsify.dom.find('pre[data-lang=markmap]');
-    // Docsify.dom.before(markmapEl, reloadBtnEl);
 
   });
 
   hook.doneEach(_ => {
-    // if (window.isRenderNote
-    //     // && !window.isShowNote
-    //     && Docsify.dom.find('pre[data-lang=markmap]')
-    // ) {
-    //   window.markmap.autoLoader.renderAll();
-    // }
-
 
     if (!window.markmap?.autoLoader?.manual) {
       if (document.readyState === 'loading')
