428df50f5f46ccebe82b663b341431af61880d10
yonatan <yonatan.lin@outlook.com>
2024-03-03 01:23:10 +0800

    chore

--- plugins/docsify-my-updater/index.js
+++ plugins/docsify-my-updater/index.js
@@ -5,8 +5,7 @@ function plugin(hook, vm) {
     let isMatched = /{docsify-my-updater}/g.test(content);
     if (isMatched) {
       let author = vm.config.name
-      let copyright = ''
-      // let copyright = `\n<p style="color:#808080;font-size:14px;margin-top:40px;">本文作者为 <a href="https://yonatan.cn">Yonatan</a>，转载请注明出处：${window.location.href}</p>`
+      let copyright = `\n<p style="color:#808080;font-size:14px;margin-top:40px;">本文作者为 <a href="https://yonatan.cn">Yonatan</a>，转载请注明出处</p>`
       let gitcus = `
         <giscus-widget
           id="comments"
