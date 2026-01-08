f58a1541b5f608b9805754481c19b6825ae30bb3
yonatan <yonatan.lin@outlook.com>
2026-01-08 17:19:59 +0800

    feat: 优化发布日期和最后修改日期

--- plugins/docsify-my-updater/index.js
+++ plugins/docsify-my-updater/index.js
@@ -1,11 +1,16 @@
 function plugin(hook, vm) {
 
   hook.beforeEach(function (content) {
+    // 匹配规则：{docsify-my-updater date:2025-01-08}
+    const reg = /{docsify-my-updater date:(\d{4}-\d{2}-\d{2})}/g;
+    const match = reg.exec(content);
     
-    let isMatched = /{docsify-my-updater}/g.test(content);
-    if (isMatched) {
+    if (match) {
+      const date = match[1];
+      let publishedDate = `<span title=${date}>${window.$docsify.formatUpdated(date)}</span>`;
       let author = vm.config.name
-      let copyright = `\n<p style="color:#808080;font-size:14px;margin-top:40px;">本文作者为 <a style="display:inline;" href="https://yonatan.cn">Yonatan</a>，转载请注明出处</p>`
+      let lastModifiedDate = `<p style="margin-top:40px;">Updated: {docsify-updated}</p>`;
+      let copyright = `<p style="color:#808080;font-size:14px;">本文作者为 <a style="display:inline;" href="https://yonatan.cn">Yonatan</a>，转载请注明出处</p>`
       let goBack = `<p>> <a style="color:#808080;" href="../">cd ..</a></p>`
       let gitcus = `
         <giscus-widget
@@ -25,11 +30,12 @@ function plugin(hook, vm) {
       // Match regex every time you start parsing .md
       let wordsCount = content.match(/([\u4e00-\u9fa5]+?|[a-zA-Z0-9]+)/g).length
       // let wordsStr = wordsCount + " words"
-      let readTime = Math.ceil(wordsCount / 400) + " min read"
+      let readTime = `<span title="${wordsCount} words">${Math.ceil(wordsCount / 400)} min read</span>`
 
+      // 2026-01-08 屏蔽用 GitHub API 获取最后修改时间
       // 使用 GitHub API 获取文件提交时间
       let isGithubFilePath = /raw.githubusercontent.com/g.test(vm.route.file)
-      if (isGithubFilePath) {
+      if (false && isGithubFilePath) {
         let pathIndex = vm.route.file.split('/').findIndex(i => i === 'blog')
         let filePath = isGithubFilePath ? vm.route.file.split('/').slice(pathIndex).join('/') : vm.route.file
         let owner = 'yonatan-d'
@@ -46,12 +52,12 @@ function plugin(hook, vm) {
             document.getElementById('last-modified').textContent = text
           })
       } else {
-        // let text = `<p style="color:#808080;font-size:14px;">${author} · {docsify-updated} · ${wordsStr} · ${readTime}</p>`
-        let text = `<p style="color:#808080;font-size:14px;">${author} · {docsify-updated} · ${readTime}</p>`
-        return content.replace(/{docsify-my-updater}/g, text) + copyright + goBack + gitcus
+        // let text = `<p style="color:#808080;font-size:14px;">${author} · ${publishedDate} · ${wordsStr} · ${readTime}</p>`
+        let text = `<p style="color:#808080;font-size:14px;">${author} · ${publishedDate} · ${readTime}</p>`
+        return content.replace(reg, text) + lastModifiedDate + copyright + goBack + gitcus
       }
 
-      return content.replace(/{docsify-my-updater}/g, '<p id="last-modified" style="color:#808080;font-size:14px;"></p>') + copyright + goBack + gitcus
+      // return content.replace(/{docsify-my-updater}/g, '<p id="last-modified" style="color:#808080;font-size:14px;"></p>') + copyright + goBack + gitcus
     }
   })
 
