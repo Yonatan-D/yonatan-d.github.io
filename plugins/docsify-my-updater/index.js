35c735e90a53b2a8d9d19035dac1702777ef1a6b
yonatan <yonatan.lin@outlook.com>
2026-01-09 17:11:20 +0800

    feat: 启用评论功能(giscus)

--- plugins/docsify-my-updater/index.js
+++ plugins/docsify-my-updater/index.js
@@ -1,9 +1,10 @@
 function plugin(hook, vm) {
 
-  hook.beforeEach(function (content) {
+  hook.beforeEach(function (markdown) {
+    // 处理日期、作者、版权信息、返回上一级、giscus、阅读时间
     // 匹配规则：{docsify-my-updater date:2025-01-08}
     const reg = /{docsify-my-updater date:(\d{4}-\d{2}-\d{2})}/g;
-    const match = reg.exec(content);
+    const match = reg.exec(markdown);
     
     if (match) {
       const date = match[1];
@@ -12,22 +13,32 @@ function plugin(hook, vm) {
       let lastModifiedDate = `<p id="last-modified" style="margin-top:40px;"></p>`;
       let copyright = `<p style="color:#808080;font-size:14px;">本文作者为 <a style="display:inline;" href="https://yonatan.cn">Yonatan</a>，转载请注明出处</p>`;
       let goBack = `<p>> <a style="color:#808080;" href="../">cd ..</a></p>`;
-      let gitcus = `
+      let giscus = `
         <giscus-widget
           id="comments"
           repo="yonatan-d/yonatan-d"
           repo-id="R_kgDOJ0TPCg"
-          category="Comment"
-          category-id="DIC_kwDOJ0TPCs4CXs2G"
-          mapping="pathname"
+          category="Announcement"
+          category-id="DIC_kwDOJ0TPCs4C0vpw"
+          mapping="og:title"
           strict="0"
           inputposition="top"
-          theme="light"
+          theme="noborder_light"
           lang="zh-CN"
         ></giscus-widget>
       `;
-      let wordsCount = content.match(/([\u4e00-\u9fa5]+?|[a-zA-Z0-9]+)/g).length;
+      let wordsCount = markdown.match(/([\u4e00-\u9fa5]+?|[a-zA-Z0-9]+)/g).length;
       let readTime = `<span title="${wordsCount} words">${Math.ceil(wordsCount / 400)} min read</span>`;
+
+      // 移除旧的 meta 标签
+      let metaOld = document.querySelector('meta[property="og:title"]');
+      if (metaOld) {
+        metaOld.remove();
+      }
+      // 设置标题meta，供 giscus 使用
+      let title = markdown.match(/# (.+)/)[1];
+      let meta = `<meta property="og:title" content="${title}">`;
+      document.head.insertAdjacentHTML('beforeend', meta);
 
       // 使用 GitHub API 获取文件提交时间
       let isGithubFilePath = /raw.githubusercontent.com/g.test(vm.route.file);
@@ -48,7 +59,7 @@ function plugin(hook, vm) {
       }
 
       let text = `<p style="color:#808080;font-size:14px;">${author} · ${publishedDate} · ${readTime}</p>`;
-      return content.replace(reg, text) + lastModifiedDate + copyright + goBack + gitcus;
+      return markdown.replace(reg, text) + lastModifiedDate + copyright + goBack + giscus;
     }
   })
 
