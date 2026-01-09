c66edf7416e210cf50aaa1df12db9387be29f1a7
yonatan <yonatan.lin@outlook.com>
2026-01-09 09:22:10 +0800

    feat: 恢复启用GitHub API获取最后修改时间；调整时间格式；
    想起来是不能屏蔽的，因为部署到静态托管网站后 {docsify-updated} 就不生效了
    参考：https://github.com/docsifyjs/docsify/issues/1157#issuecomment-890467244

--- plugins/docsify-my-updater/index.js
+++ plugins/docsify-my-updater/index.js
@@ -8,10 +8,10 @@ function plugin(hook, vm) {
     if (match) {
       const date = match[1];
       let publishedDate = `<span title=${date}>${window.$docsify.formatUpdated(date)}</span>`;
-      let author = vm.config.name
-      let lastModifiedDate = `<p style="margin-top:40px;">Updated: {docsify-updated}</p>`;
-      let copyright = `<p style="color:#808080;font-size:14px;">本文作者为 <a style="display:inline;" href="https://yonatan.cn">Yonatan</a>，转载请注明出处</p>`
-      let goBack = `<p>> <a style="color:#808080;" href="../">cd ..</a></p>`
+      let author = vm.config.name;
+      let lastModifiedDate = `<p id="last-modified" style="margin-top:40px;"></p>`;
+      let copyright = `<p style="color:#808080;font-size:14px;">本文作者为 <a style="display:inline;" href="https://yonatan.cn">Yonatan</a>，转载请注明出处</p>`;
+      let goBack = `<p>> <a style="color:#808080;" href="../">cd ..</a></p>`;
       let gitcus = `
         <giscus-widget
           id="comments"
@@ -25,39 +25,30 @@ function plugin(hook, vm) {
           theme="light"
           lang="zh-CN"
         ></giscus-widget>
-      `
+      `;
+      let wordsCount = content.match(/([\u4e00-\u9fa5]+?|[a-zA-Z0-9]+)/g).length;
+      let readTime = `<span title="${wordsCount} words">${Math.ceil(wordsCount / 400)} min read</span>`;
 
-      // Match regex every time you start parsing .md
-      let wordsCount = content.match(/([\u4e00-\u9fa5]+?|[a-zA-Z0-9]+)/g).length
-      // let wordsStr = wordsCount + " words"
-      let readTime = `<span title="${wordsCount} words">${Math.ceil(wordsCount / 400)} min read</span>`
-
-      // 2026-01-08 屏蔽用 GitHub API 获取最后修改时间
       // 使用 GitHub API 获取文件提交时间
-      let isGithubFilePath = /raw.githubusercontent.com/g.test(vm.route.file)
-      if (false && isGithubFilePath) {
-        let pathIndex = vm.route.file.split('/').findIndex(i => i === 'blog')
-        let filePath = isGithubFilePath ? vm.route.file.split('/').slice(pathIndex).join('/') : vm.route.file
-        let owner = 'yonatan-d'
-        let repo = 'yonatan-d'
-        let date_url = `https://api.github.com/repos/${owner}/${repo}/commits?per_page=1&path=${filePath}`
+      let isGithubFilePath = /raw.githubusercontent.com/g.test(vm.route.file);
+      if (isGithubFilePath) {
+        let pathIndex = vm.route.file.split('/').findIndex(i => i === 'blog');
+        let filePath = isGithubFilePath ? vm.route.file.split('/').slice(pathIndex).join('/') : vm.route.file;
+        let owner = 'yonatan-d';
+        let repo = 'yonatan-d';
+        let date_url = `https://api.github.com/repos/${owner}/${repo}/commits?per_page=1&path=${filePath}`;
         fetch(date_url)
           .then((response) => {
-            return response.json()
+            return response.json();
           })
           .then((commits) => {
-            let date = commits[0]['commit']['committer']['date'];
-            let commitDate = window.$docsify.formatUpdated(date);
-            let text = `${author} · ${commitDate} · ${readTime}`
-            document.getElementById('last-modified').textContent = text
+            let modified = commits[0]['commit']['committer']['date'].slice(0, 10);
+            document.getElementById('last-modified').textContent = 'Updated: ' + modified;
           })
-      } else {
-        // let text = `<p style="color:#808080;font-size:14px;">${author} · ${publishedDate} · ${wordsStr} · ${readTime}</p>`
-        let text = `<p style="color:#808080;font-size:14px;">${author} · ${publishedDate} · ${readTime}</p>`
-        return content.replace(reg, text) + lastModifiedDate + copyright + goBack + gitcus
       }
 
-      // return content.replace(/{docsify-my-updater}/g, '<p id="last-modified" style="color:#808080;font-size:14px;"></p>') + copyright + goBack + gitcus
+      let text = `<p style="color:#808080;font-size:14px;">${author} · ${publishedDate} · ${readTime}</p>`;
+      return content.replace(reg, text) + lastModifiedDate + copyright + goBack + gitcus;
     }
   })
 
